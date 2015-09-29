4d467d2cc7871900e401f196ecb6f44e75e42e4b
mohitc@live.de <mohitc@live.de>
2015-09-30 00:11:43 +0200

    initialize Notification Manager from File

--- TopologyElements/src/com/helpers/notification/manager/NotificationManager.java
+++ TopologyElements/src/com/helpers/notification/manager/NotificationManager.java
@@ -1,8 +1,11 @@
 package com.helpers.notification.manager;
 
 import com.helpers.notification.filters.NotificationFilter;
+import com.helpers.notification.filters.impl.TypeNotificationFilter;
 import com.helpers.notification.handlers.NotificationHandler;
+import com.helpers.notification.handlers.impl.LogNotificationHandler;
 import com.helpers.notification.messages.NotificationMessage;
+import com.helpers.notification.messages.NotificationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +35,34 @@ public class NotificationManager {
     notificationProcessors.add(processor);
     ExecutorService executor = Executors.newSingleThreadExecutor();
     executor.submit(processor);
+  }
+
+  static {
+    //load notification handlers
+    List<String> classNames = new ArrayList<>();
+    classNames.add("com.helpers.notification.handlers.log.LogNotificationHandler");
+    for (String in: classNames) {
+      Class className = null;
+      try {
+        className = Class.forName(in);
+      } catch (ClassNotFoundException e) {
+        log.error("Could not find instance of Notification Handler class", e);
+      }
+      if (className!=null) {
+        if (!(NotificationHandler.class.isAssignableFrom(className))) {
+          log.error("Class " + className.getSimpleName() + " not an instance of a NotificationHandler");
+        } else {
+          try {
+            NotificationHandler hdlr = (NotificationHandler)className.newInstance();
+            List<NotificationFilter> filters = new ArrayList<>();
+            filters.add(new TypeNotificationFilter(NotificationType.TENotification));
+            NotificationManager.addNotificationProcessor(filters, hdlr);
+          } catch (InstantiationException | IllegalAccessException e) {
+            log.error("Could not instantiate class. Please check if default constructor is implemented", e);
+          }
+        }
+      }
+    }
   }
 
 }
